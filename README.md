b9cae42d2027f29b609b7571d2ee18868f4e06a2
I.Mihara <issei.mihara@xforestx.com>
2023-04-19 13:43:42 +0000

    added more details

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # DemultiplexJulia
+DemultiplexJulia is a Julia package that provides functions for demultiplexing reads based on barcodes. The package includes two functions: "Demultiplexer" and "BBCSemiglobalAlignmentScore".
+## Installation
+To install the DemultiplexJulia package, open the Julia REPL and run the following command:
+```Julia
+using Pkg;Pkg.add DemultiplexJulia
+```
+## Functions
+### Demultiplexer
+The "Demultiplexer" function takes four input arguments:"input","input2","bbc_tsv",and"output_dir",and an optional fifth argument "maximum_errorrate". The function reads in fastq files compares the barcode sequences to a reference file, and outputs files containing the reads that match the reference barcodes.
+```julia
+function Demultiplexer(input::String,input2::String,bbc_tsv::String,output_dir::String,maximum_errorrate=0.22::Float64)
+```
+
+#### Arguments
+* `input::String`:the path to the input R1 fastq file
+* `input2::String`:the path to the input R2 fastq file
+* `bbc_tsv::String`:the path to the reference barcode file
+* `output_dir::String`: the path to the output directory where the demultiplexed files will be written.
+* `maximum_errorate=0.22::Float64`:an optional argument that sets the maximum error rate for barcode matching. The default value is 0.22
+
+### BBCSemiglobalAlignmentScore
+The "BBCSemiglobalAlignmentScore"function takes threee input arguments:"ref","query",and"maximum_errorrate".The function calculates a semi-global alignment score between the two input sequences,"ref" and "query", using an affine gap score model. It then computes the error rate of the alignment and adjusts the score based on the number of matches, mismatches, and insertions. If the error rate is greater than the maximum allowed error rate, the score is set to zero. The function returns the final alignment score.
+
+```julia
+function BBCSemiglobalAlignmentScore(ref::String,query::String,maximum_errorrate::Float64)
+```
+#### arguments
+* `ref`:the reference sequence.
+* `query`:the query sequence to be aligned to the reference.
+* `maximum_errorrate`:the maximum allowed rate for barcode matching.
+
+## Usage
+here is an example of how to use the "Demultiplexer" function
+```julia
+using DemultiplexJulia
+
+input = "path/to/input.fastq"
+input2 = "path/to/input2.fastq"
+bbc_tsv = "path/to/barcodes.tsv"
+output_dir = "path/to/output/directory"
+maximum_errorrate = 0.2
+
+Demultiplexer(input, input2, bbc_tsv, output_dir, maximum_errorrate)
+```
+## License
+???
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,82 @@
 using DemultiplexJulia
 using Test
 
+
+function test_Demultiplexer()
+    # Test inputs
+    tmpdir = mktempdir(cleanup=true)
+
+    input = joinpath(tmpdir,"test_input.fastq")
+    input2 = joinpath(tmpdir,"test_input2.fastq")
+    bbc_tsv = joinpath(tmpdir,"test_bbc.tsv")
+    output_dir = joinpath(tmpdir,"test_output")
+
+    open(input, "w") do io
+        println(io, "@example_read1")
+        println(io, "AATTATATGCCGCCATATT")
+        println(io, "+")
+        println(io, rand('A':'J', 19))
+    end
+
+    open(input2, "w") do io
+        println(io, "@example_read1")
+        println(io, "ATGC")
+        println(io, "+")
+        println(io, rand('A':'J', 4))
+    end
+    
+    io = open(filename, "w")
+    data = [
+    ["ID","Number","Full_seq","Full_annotation"],
+    ["sample_1", 1, "AAAGGGGGAAA", "LLLBBBBB333"],
+    ["sample_2", 2, "AAAAGCCGCCAAA","LLLLBBBBBB333"],
+    ["sample_3", 3, "AAAGGCGGCAA","LLLBBBBBB33"]
+    ]
+    for row in data
+        row_str = join(string.(row), "\t")
+        println(io, row_str)
+    end
+    close(io)
+    # Run function
+    Demultiplexer(input, input2, bbc_tsv, output_dir, 0.22)
+    
+    # Test outputs
+    @test isdir(output_dir)
+    @test !isfile(joinpath(output_dir, "trimmed-unknown.fastq"))
+    @test !isfile(joinpath(output_dir, "trimmed-sample_1.fastq"))
+    @test isfile(joinpath(output_dir, "trimmed-sample_2.fastq"))
+    @test !isfile(joinpath(output_dir, "trimmed-sample_3.fastq"))
+end
+
+function test_BBCSemiglobalAlignmentScore()
+    ref = "AAAAAGGAA"
+    query = "GG"
+    maximum_errorrate = 0.22
+    
+    score = BBCSemiglobalAlignmentScore(ref, query, maximum_errorrate)
+    @test score == 2
+
+    ref = "TTTTGGGGCCCGAAATGTGAAATAAGGGTA"
+    query = "CCCGAA"
+    score = BBCSemiglobalAlignmentScore(ref, query, maximum_errorrate)
+    @test score == 6
+    
+    ref = "TATATTCGCGGCATCCGCACTTATGCGCGAATGCATAGCCAACTGCTTGGCAGTTGGCTTCAGACCA"
+    query = "AGCCAACTGCTTGGCAGTTGGC"
+    score = BBCSemiglobalAlignmentScore(ref, query, maximum_errorrate)
+    @test score == 22
+
+    ref = "TATATTCGCGGCATCCGCACTTATGCGCGAATGCATAGCCAACTGCTTGCAGTTGGCTTCAGACCA"
+    score = BBCSemiglobalAlignmentScore(ref, query, maximum_errorrate)
+    @test score == 19
+end
+
 @testset "DemultiplexJulia.jl" begin
-    # Write your tests here.
-    @test BBCSemiglobalAlignmentScore("AAAAAGGAA","GG",0.22) == 2
-    @test BBCSemiglobalAlignmentScore("TTTTGGGGCCCGAAATGTGAAATAAGGGTA","CCCGAA",0.22) == 6
-    @test BBCSemiglobalAlignmentScore("TATATTCGCGGCATCCGCACTTATGCGCGAATGCATAGCCAACTGCTTGGCAGTTGGCTTCAGACCA","AGCCAACTGCTTGGCAGTTGGC",0.22) == 22
-    @test BBCSemiglobalAlignmentScore("TATATTCGCGGCATCCGCACTTATGCGCGAATGCATAGCCAACTGCTTGGCAGTTGGCTTCAGACCA","AGCCAACTGCTGGCAGTTGGC",0.22) == 19
-    @test BBCSemiglobalAlignmentScore("TATATTCGCGGCATCCGCACTTATGCGCGAATGCATAGCCAACTGCTTGGCAGTTGGCTTCAGACCA","AGCCAACTGCTGGCAGTTGGC",0.22)     
+    @testset "Demultiplexer tests" begin
+        @testset "test_Demultiplexer" test_Demultiplexer()
+    end
+    
+    @testset "BBCSemiglobalAlignmentScore tests" begin
+        @testset "test_BBCSemiglobalAlignmentScore" test_BBCSemiglobalAlignmentScore()
+    end
 end
