1b621435f66547a43007981a7316f45bfbf5ca6a
I.Mihara <issei.mihara@xforestx.com>
2023-04-26 11:21:10 +0000

    made minor modifications

--- src/DemultiplexJulia.jl
+++ src/DemultiplexJulia.jl
@@ -1,10 +1,10 @@
 module DemultiplexJulia
 
-export 
-BBCSemiglobalAlignmentScore,
-Demultiplexer
+export
+    BBCSemiglobalAlignmentScore,
+    Demultiplex
 
-using BioAlignments , CSV , DataFrames
+using BioAlignments, CSV, DataFrames
 
 """
     BBCSemiglobalAlignmentScore(ref::String,query::String,maximum_errorrate=0.22::Float64) -> score::Int64 
@@ -13,13 +13,13 @@ using the Bioalignments.jl package and return score based on the number of mach,
 If the error rate between the aligned seqences is greater than the maximum allowed error rate,
 the function returns a score of 0.
 """
-function BBCSemiglobalAlignmentScore(ref::String,query::String,maximum_errorrate=0.22::Float64)
+function BBCSemiglobalAlignmentScore(ref::String, query::String, maximum_errorrate=0.22::Float64)
     problem = SemiGlobalAlignment()
-    scoremodel = AffineGapScoreModel(match=0,mismatch=-1,gap_open=0,gap_extend=-1)
-    result=pairalign(problem, query, ref, scoremodel)
+    scoremodel = AffineGapScoreModel(match=0, mismatch=-1, gap_open=0, gap_extend=-1)
+    result = pairalign(problem, query, ref, scoremodel)
     alignment_result = alignment(result)
-    errorrate = - BioAlignments.score(result) / length(query)
-    score = count_matches(alignment_result)-count_mismatches(alignment_result)-2*count_insertions(alignment_result)
+    errorrate = -BioAlignments.score(result) / length(query)
+    score = count_matches(alignment_result) - count_mismatches(alignment_result) - 2 * count_insertions(alignment_result)
     if errorrate > maximum_errorrate
         score = 0
     end
@@ -27,47 +27,47 @@ function BBCSemiglobalAlignmentScore(ref::String,query::String,maximum_errorrate
 end
 
 """
-    function Demultiplexer(input::String,input2::String,bbc_tsv::String,output_dir::String,maximum_errorrate=0.22::Float64)
+    function Demultiplex(input::String,input2::String,bbc_tsv::String,output_dir::String,maximum_errorrate=0.22::Float64)
 Demultiplex reads based on barcode sequences, with the option to set a maximum error rate for matching.
 """
-function Demultiplexer(input::String,input2::String,bbc_tsv::String,output_dir::String,maximum_errorrate=0.22::Float64)
+function Demultiplex(input::String, input2::String, bbc_tsv::String, output_dir::String, maximum_errorrate=0.22::Float64)
     if isdir(output_dir)
-        rm(output_dir,recursive=true) 
+        rm(output_dir, recursive=true)
     end
     mkdir(output_dir)
-    bbc_df = CSV.read(bbc_tsv,DataFrame,delim="\t")
-    prefix_region = 1:findfirst('B',bbc_df.Full_annotation[1])-1
-    suffix_region = findlast('B',bbc_df.Full_annotation[1])+1:length(bbc_df.Full_annotation[1])
-    prefix = SubString(bbc_df.Full_seq[1],prefix_region)
-    suffix = SubString(bbc_df.Full_seq[1],suffix_region)
-    bbc_df.Full_seq = replace.(bbc_df.Full_seq,prefix=>"")
-    bbc_df.Full_seq = replace.(bbc_df.Full_seq,suffix=>"")
-    bbc_df.Full_seq = replace.(bbc_df.Full_seq,"U"=>"T")
+    bbc_df = CSV.read(bbc_tsv, DataFrame, delim="\t")
+    for i in 1:nrow(bbc_df)
+    prefix_region = 1:findfirst('B', bbc_df.Full_annotation[i])-1
+    suffix_region = findlast('B', bbc_df.Full_annotation[i])+1:length(bbc_df.Full_annotation[i])
+    prefix = SubString(bbc_df.Full_seq[i], prefix_region)
+    suffix = SubString(bbc_df.Full_seq[i], suffix_region)
+    bbc_df.Full_seq[i] = replace(bbc_df.Full_seq[i], prefix => "")
+    bbc_df.Full_seq[i] = replace(bbc_df.Full_seq[i], suffix => "")
+    end
+    bbc_df.Full_seq = replace.(bbc_df.Full_seq, "U" => "T")
     bbc_df.Full_seq = reverse.(bbc_df.Full_seq)
-    bbc_df.Full_seq = replace.(bbc_df.Full_seq,"A"=>"T","T"=>"A","G"=>"C","C"=>"G")
-    open(input,"r") do file
+    bbc_df.Full_seq = replace.(bbc_df.Full_seq, "A" => "T", "T" => "A", "G" => "C", "C" => "G")
+    open(input, "r") do file
         mode = ""
         header = ""
         filename = ""
         seq = ""
         line3 = ""
         quality_score = ""
-        open(input2,"r")do file2
-            k = 0
+        open(input2, "r") do file2
             for line in eachline(file)
-                R2=readline(file2)
+                R2 = readline(file2)
                 if line[1] == '@' && mode == ""
                     header = R2
                     mode = "header"
                 elseif mode == "header"
-                    k+=1
                     seq = line
                     mode = "line3"
-                    maximum_bbc_score=0
-                    maximum_bbc_score_number=0
-                    for (i, row) in enumerate( eachrow( bbc_df ) ) 
-                        bbc_score = BBCSemiglobalAlignmentScore(seq,row.Full_seq,maximum_errorrate)
-                        if maximum_bbc_score<bbc_score
+                    maximum_bbc_score = 0
+                    maximum_bbc_score_number = 0
+                    for (i, row) in enumerate(eachrow(bbc_df))
+                        bbc_score = BBCSemiglobalAlignmentScore(seq, row.Full_seq, maximum_errorrate)
+                        if maximum_bbc_score < bbc_score
                             maximum_bbc_score = bbc_score
                             maximum_bbc_score_number = i
                         end
@@ -75,17 +75,17 @@ function Demultiplexer(input::String,input2::String,bbc_tsv::String,output_dir::
                     seq = R2
                     if maximum_bbc_score_number == 0
                         filename = output_dir * "/trimmed-unknown.fastq"
-                    else 
-                        filename = output_dir*"/trimmed-"*string(bbc_df.ID[maximum_bbc_score_number])*".fastq"    
+                    else
+                        filename = output_dir * "/trimmed-" * string(bbc_df.ID[maximum_bbc_score_number]) * ".fastq"
                     end
                 elseif mode == "line3"
                     line3 = R2
                     mode = "quality_score"
-                elseif mode =="quality_score"
+                elseif mode == "quality_score"
                     quality_score = R2
-                    open(filename,"a")do outputfile
-                        write(outputfile,header*"\n"*seq*"\n"*line3*"\n"*quality_score*"\n")
-                    end 
+                    open(filename, "a") do outputfile
+                        write(outputfile, header * "\n" * seq * "\n" * line3 * "\n" * quality_score * "\n")
+                    end
                     mode = ""
                 end
             end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,18 +2,22 @@ using DemultiplexJulia
 using Test
 
 
-function test_Demultiplexer()
+function test_Demultiplex()
     # Test inputs
     tmpdir = mktempdir()
 
-    input = joinpath(tmpdir,"test_input.fastq")
-    input2 = joinpath(tmpdir,"test_input2.fastq")
-    bbc_tsv = joinpath(tmpdir,"test_bbc.tsv")
-    output_dir = joinpath(tmpdir,"test_output")
+    input = joinpath(tmpdir, "test_input.fastq")
+    input2 = joinpath(tmpdir, "test_input2.fastq")
+    bbc_tsv = joinpath(tmpdir, "test_bbc.tsv")
+    output_dir = joinpath(tmpdir, "test_output")
 
     open(input, "w") do io
         println(io, "@example_read1")
         println(io, "AATTATATGGGGGGATATT")
+        println(io, "+")
+        println(io, rand('A':'J', 19))
+        println(io, "@example_read2")
+        println(io, "AATTATATGCCGCCATATT")
         println(io, "+")
         println(io, rand('A':'J', 19))
     end
@@ -22,15 +26,19 @@ function test_Demultiplexer()
         println(io, "@example_read1")
         println(io, "ATGC")
         println(io, "+")
-        println(io, rand('A':'J', 4))
+        println(io, "FFFF")
+        println(io, "@example_read2")
+        println(io, "TTTTT")
+        println(io, "+")
+        println(io, "FFF:F")
     end
-    
+
     io = open(bbc_tsv, "w")
     data = [
-    ["ID","Number","Full_seq","Full_annotation"],
-    ["sample_1", 1, "AAAGGGGGGAAA", "LLLBBBBBB333"],
-    ["sample_2", 2, "AAACCCCCCAAA","LLLBBBBBB333"],
-    ["sample_3", 3, "AAAGGCGGCAAA","LLLBBBBBB333"]
+        ["ID", "Number", "Full_seq", "Full_annotation"],
+        ["sample_1", 1, "AAAGGGGGGAAAT", "LLLBBBBBB3333"],
+        ["sample_2", 2, "AACCCCCCAAA", "LLBBBBBB333"],
+        ["sample_3", 3, "AAAGGCGGCAT", "LLLBBBBBB33"]
     ]
     for row in data
         row_str = join(string.(row), "\t")
@@ -39,21 +47,33 @@ function test_Demultiplexer()
     close(io)
 
     # Run function
-    Demultiplexer(input, input2, bbc_tsv, output_dir, 0.22)
-    
-    # Test outputs
+    Demultiplex(input, input2, bbc_tsv, output_dir, 0.22)
+
+    # Test outputsq
     @test isdir(output_dir)
     @test !isfile(joinpath(output_dir, "trimmed-unknown.fastq"))
     @test !isfile(joinpath(output_dir, "trimmed-sample_1.fastq"))
     @test isfile(joinpath(output_dir, "trimmed-sample_2.fastq"))
-    @test !isfile(joinpath(output_dir, "trimmed-sample_3.fastq"))
+    @test isfile(joinpath(output_dir, "trimmed-sample_3.fastq"))
+    open(joinpath(output_dir, "trimmed-sample_2.fastq"),"r") do out
+        @test readline(out) == "@example_read1"
+        @test readline(out) == "ATGC"
+        @test readline(out) == "+"
+        @test readline(out) == "FFFF"
+    end
+    open(joinpath(output_dir, "trimmed-sample_3.fastq"),"r") do out
+        @test readline(out) == "@example_read2"
+        @test readline(out) == "TTTTT"
+        @test readline(out) == "+"
+        @test readline(out) == "FFF:F"
+    end
 end
 
 function test_BBCSemiglobalAlignmentScore()
     ref = "AAAAAGGAA"
     query = "GG"
     maximum_errorrate = 0.22
-    
+
     score = BBCSemiglobalAlignmentScore(ref, query, maximum_errorrate)
     @test score == 2
 
@@ -61,7 +81,7 @@ function test_BBCSemiglobalAlignmentScore()
     query = "CCCGAA"
     score = BBCSemiglobalAlignmentScore(ref, query, maximum_errorrate)
     @test score == 6
-    
+
     ref = "TATATTCGCGGCATCCGCACTTATGCGCGAATGCATAGCCAACTGCTTGGCAGTTGGCTTCAGACCA"
     query = "AGCCAACTGCTTGGCAGTTGGC"
     score = BBCSemiglobalAlignmentScore(ref, query, maximum_errorrate)
@@ -73,10 +93,10 @@ function test_BBCSemiglobalAlignmentScore()
 end
 
 @testset "DemultiplexJulia.jl" begin
-    @testset "Demultiplexer tests" begin
-        test_Demultiplexer()
+    @testset "Demultiplex tests" begin
+        test_Demultiplex()
     end
-    
+
     @testset "BBCSemiglobalAlignmentScore tests" begin
         test_BBCSemiglobalAlignmentScore()
     end
